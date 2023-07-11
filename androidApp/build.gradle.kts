f8fcb23f0e0d4b21d3de0911077574d1aa70247f
PierreVieira <pierrevieiraggg@gmail.com>
2023-07-11 00:28:18 -0300

    update kotlin version: 1.8.20 -> 1.8.22

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
     packaging {
         resources {
@@ -45,5 +45,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
     implementation("androidx.compose.foundation:foundation:1.4.3")
     implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
 }
